01443921b43ea906d18083d17b0ffa231c5f983a
PEBU3OP1 <sevakuzya@mail.ru>
2024-03-28 21:05:27 +0300

    Hw4 almost

---
+++ HW4/HW4.py
@@ -0,0 +1,50 @@
+"""
+Задание №7
+� Напишите программу на Python, которая будет находить
+сумму элементов массива из 1000000 целых чисел.
+� Пример массива: arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...]
+� Массив должен быть заполнен случайными целыми числами
+от 1 до 100.
+� При решении задачи нужно использовать многопоточность,
+многопроцессорность и асинхронность.
+� В каждом решении нужно вывести время выполнения
+вычислений.
+"""
+
+import random
+import threading
+import time
+import multiprocessing
+
+
+def ar_sum():
+    summary = 0
+    for _ in range(1_000_000):
+        summary = summary + random.randint(1,100)
+    print(summary)
+    return
+
+#################Syncr######################
+start_syncr = time.time()
+for i in range(4):
+    ar_sum()
+print(f'Syncr {time.time() - start_syncr }')
+
+
+#################Threads######################
+threads = []
+
+
+start_threads = time.time()
+for i in range(4):
+    t = threading.Thread(target=ar_sum)
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+print(f'Threads {time.time() - start_threads }')
+
+
+
+
